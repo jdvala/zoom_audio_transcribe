005880cdc2cec3f1001d525e6cd0fc4f9525226b
Jay Vala <24193355+jdvala@users.noreply.github.com>
2020-09-13 21:08:56 +0200

    :pencil: update readme, for pypi

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # zoom_audio_transcribe
 ![GH](https://github.com/jdvala/zoom-audio-transcribe/workflows/GH/badge.svg)
-[![Code Coverage](https://codecov.io/gh/jdvala/zoom_audio_transcribe/branch/master/graph/badge.svg)](https://codecov.io/gh/jdvala/zoom_audio_transcribe)
+![Licence](https://img.shields.io/github/license/jdvala/zoom_audio_transcribe)
+[![pypi Version](https://img.shields.io/pypi/v/pipgrip.svg?logo=pypi&logoColor=white)](https://pypi.org/project/zoom-audio-transcribe/  )
+[![python](https://img.shields.io/pypi/pyversions/zoom-audio-transcribe)](https://pypi.org/project/zoom-audio-transcribe/)
+[![black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/python/black)
+![OS](https://img.shields.io/badge/OS-Linux-organe)
+
 
 Transcribe zoom meetings audio locally.
 
@@ -27,6 +32,8 @@ $ zoom_audio_transcribe
 Once you are at the menu, select the appropiate meeting and let it run.
 
 ![Done](https://github.com/jdvala/zoom-audio-transcribe/blob/master/screenshots/done.png)
+
+
 
 Once the transcription is done the, file will be saved in the same folder as the zoom meetings recording. For linux users its `/home/$USER/Documents/Zoom/{meeting_which_was_selected}
 ---
--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ from setuptools import find_packages, setup
 here = path.abspath(path.dirname(__file__))
 
 requirements_path = path.join(here, "requirements", "prod.txt")
+readme_path = path.join(here, "README.md")
 
 
 def read_requirements(path):
@@ -16,11 +17,18 @@ def read_requirements(path):
         raise RuntimeError("{} is broken".format(path))
 
 
+def read_readme(path):
+    with io.open(path, mode="rt", encoding="utf-8") as fp:
+        return fp.read()
+
+
 setup(
     name="zoom_audio_transcribe",
     python_requires=">=3.6",
     setup_requires=["setuptools_scm"],
     install_requires=read_requirements(requirements_path),
+    long_description=read_readme(readme_path),
+    long_description_content_type="text/markdown",
     use_scm_version={
         "version_scheme": "guess-next-dev",
         "local_scheme": "dirty-tag",
@@ -38,9 +46,8 @@ setup(
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Environment :: Console",
-        "Operating System :: OS Independent",
+        "Operating System :: linux",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
